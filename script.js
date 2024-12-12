becc6ea884cb933968084ce52e2a77c70f41c3aa
gi <135059916+giovannalimang@users.noreply.github.com>
2024-12-12 08:13:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -712,13 +712,3 @@ function AcharReserva(NumQuarto, id, tipo) {
     }
     return null
 }
-
-
-document.querySelectorAll(".mudar-formulario").forEach((elemento) => {
-    elemento.addEventListener("click", () => {
-        document.getElementById("login").classList.toggle("formulario-visivel");
-        document.getElementById("registro").classList.toggle("formulario-visivel");
-        document.getElementById("mensagemLogin").classList.toggle("formulario-visivel");
-        document.getElementById("mensagemRegistro").classList.toggle("formulario-visivel");
-    });
-});
